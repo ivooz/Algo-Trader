168bda38733e51f24074839554bd9c3ada0b7b33
unknown <dkwi@PCLLE107.gft.com>
2015-10-14 13:02:32 +0200

    Added TickerHintsForNewTickerAdding

--- src/main/java/com/gft/repository/data/StockRepository.java
+++ src/main/java/com/gft/repository/data/StockRepository.java
@@ -1,6 +1,11 @@
 package com.gft.repository.data;
 
+import java.util.List;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gft.model.db.Stock;
@@ -10,5 +15,8 @@ import com.gft.model.db.Stock;
  */
 @Repository
 public interface StockRepository extends JpaRepository<Stock, String> {
-
+public static final String Querys = "SELECT s.ticker FROM Stock s";
+	@Query(Querys)
+	@Fetch(FetchMode.SELECT)
+	public List<String> findTickers();
 }
