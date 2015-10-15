ff174a8423fa8329848a7750ffc2136574927dbe
unknown <mnwi@PCLLE126.gft.com>
2015-10-15 16:25:39 +0200

    After merging

--- src/main/java/com/gft/rest/TickersRest.java
+++ src/main/java/com/gft/rest/TickersRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gft.service.NewTickerHinter;
+import com.gft.service.downloading.NewTickerHinter;
 import com.gft.service.parsing.ParsingException;
 
 @RestController
